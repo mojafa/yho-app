7e4f688b83fd9576bb6dc2f5c43ab34e830bee6a
mojafa <jmohsa@gmail.com>
2023-06-27 23:18:33 +0300

    "initial commit"

--- yho-app/ContentView.swift
+++ yho-app/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView(content: {
+
+            ZStack {
+                VStack {
+                    Text("WELCOME TO YHO BANK!")
+                        .foregroundStyle(.white)
+                    Spacer()
+                    Text("Managing Yho money is easy with Yho!")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+                       
+                    Spacer()
+                    HStack{
+                        Button("LOGIN", role: .none) {
+                        }
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .border(Color.black, width: 10)
+                        .cornerRadius(20)
+                        
+                        Button("SIGNUP", role: .none) {
+                        }
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .border(Color.white, width: 10)
+                        .cornerRadius(20)
+                        
+                    }
+                   
+
+                }
+               
+                    }.background(Color(red:57/255,green:96/255,blue:215/255))
+            Spacer()
+           
+            
+            
+            })
+        
     }
 }
 
