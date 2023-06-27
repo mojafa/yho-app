10bf9bee27eec42a6f25eb2235c2f89335e7069a
mojafa <jmohsa@gmail.com>
2023-06-27 22:40:06 +0300

    Initial Commit

---
+++ yho-app/yho_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  yho_appApp.swift
+//  yho-app
+//
+//  Created by Jafa on 27/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct yho_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
