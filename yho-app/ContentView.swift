d44fd12233687843a1102430b454835789881faa
AhmedARmohamed <mahmedrashid128@gmail.com>
2023-06-28 21:19:04 +0300

    Implemeted the tab view and the screens for onboarding

--- yho-app/ContentView.swift
+++ yho-app/ContentView.swift
@@ -9,47 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView(content: {
-
-            ZStack {
-                VStack {
-                    Text("WELCOME TO YHO BANK!")
-                        .foregroundStyle(.white)
-                    Spacer()
-                    Text("Managing Yho money is easy with Yho!")
-                        .font(.title)
-                        .fontWeight(.heavy)
-                        .foregroundStyle(.white)
-                       
-                    Spacer()
-                    HStack{
-                        Button("LOGIN", role: .none) {
-                        }
-                        .padding()
-                        .background(Color.black)
-                        .foregroundColor(.white)
-                        .border(Color.black, width: 10)
-                        .cornerRadius(20)
-                        
-                        Button("SIGNUP", role: .none) {
-                        }
-                        .padding()
-                        .background(Color.white)
-                        .foregroundColor(.black)
-                        .border(Color.white, width: 10)
-                        .cornerRadius(20)
-                        
-                    }
-                   
-
-                }
-               
-                    }.background(Color(red:57/255,green:96/255,blue:215/255))
-            Spacer()
-           
-            
-            
-            })
+//        Home()
+        OnboardingScreen()
         
     }
 }
@@ -59,3 +20,92 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Home Screen
+struct Home: View {
+    var body: some View {
+        VStack {
+            Text("YHO Bank")
+                .font(.title)
+                .fontWeight(.heavy)
+        }
+    }
+}
+
+struct OnboardingScreen: View {
+    let screens = [
+        OnboardingView(imageName: "screen1", title: "Managing money is easy with Yho!", backgroundColor: Color(red:57/255,green:96/255,blue:215/255)),
+        OnboardingView(imageName: "screen2", title: "Get a new Card with free delivery", backgroundColor: Color(red:118/255,green:99/255,blue:230/255)),
+        OnboardingView(imageName: "screen3", title: "Spend Smarter everyday. Ready?", backgroundColor: Color(red:57/255,green:96/255,blue:215/255))
+    ]
+
+    var body: some View {
+            ZStack {
+                Spacer()
+                
+                TabView {
+                    ForEach(screens, id: \.title) { screen in
+                        screen
+                    }
+                    
+                }
+                .frame(maxHeight: .infinity)
+                .tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+               
+        }
+            .ignoresSafeArea(.all)
+        
+    }
+}
+
+struct OnboardingView: View {
+    let imageName: String
+    let title: String
+    let backgroundColor: Color
+
+    var body: some View {
+        ZStack {
+        VStack {
+            Spacer(minLength: 0)
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .kerning(1.5)
+            Spacer(minLength: 0)
+            
+            HStack {
+                Button("Login", role: .none) {
+                }
+                .foregroundColor(.white)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding()
+                .background(Color.black)
+                .cornerRadius(40)
+                
+                
+                Button("Signup", role: .none) {
+                }
+                .fontWeight(.semibold)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .foregroundColor(.black)
+                .padding()
+                .background(Color.white)
+                .cornerRadius(40)
+                
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 50)
+            
+        }
+        .background(backgroundColor)
+    }
+        
+
+    }
+}
+
+
+
+
